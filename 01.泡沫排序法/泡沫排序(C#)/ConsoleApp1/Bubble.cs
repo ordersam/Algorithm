4e81f950b52437f18ea4ad9da5a1c26bd51800cf
ordersam <46249762+ordersam@users.noreply.github.com>
2020-09-17 07:15:19 +0800

    泡沫排序(C#、javascript、python)

---
+++ 01.泡沫排序法/泡沫排序(C#)/ConsoleApp1/Bubble.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Bubble
+    {
+        public static string bubble1(int[] array1)
+        {
+            int temp = 0;
+            for (int i = 0; i < array1.Length - 1; i++)
+            {
+                for (int j = 0; j < array1.Length -1 - i; j++)
+                {
+                    if (array1[j] > array1[j+1])
+                    {
+                        temp = array1[j];
+                        array1[j] = array1[j+1];
+                        array1[j+1] = temp;
+                    }
+                }
+            }
+
+            string strArray = "";
+            foreach (int element in array1)
+            {
+                strArray += element.ToString() + " ";
+            }
+            return strArray;
+        }
+
+        public static string bubble2(int[] array1)
+        {
+            int temp = 0;
+            int i = 0;
+            while (i < array1.Length - 1)
+            {
+                for (int j = 0; j < array1.Length - 1 - i; j++)
+                {
+                    if (array1[j] > array1[j + 1])
+                    {
+                        temp = array1[j];
+                        array1[j] = array1[j + 1];
+                        array1[j + 1] = temp;
+                    }
+                }
+                i++;
+            }
+
+            string strArray = "";
+            foreach (int element in array1)
+            {
+                strArray += element.ToString() + " ";
+            }
+            return strArray;
+        }
+    }
+}
---
+++ 01.泡沫排序法/泡沫排序(C#)/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 12, 9, 6, 2, 4, 1, 11, 8, 3, 5, 7, 10, 13 };
+            string bubble1 = Bubble.bubble1(array1);
+            Console.WriteLine(bubble1);
+
+            int[] array2 = new int[] { 11, 9, 6, 2, 4, 1, 12, 8, 3, 5, 7, 10, 13 };
+            string bubble2 = Bubble.bubble1(array2);
+            Console.WriteLine(bubble2);
+
+            Console.Read();
+        }
+    }
+}
