f4b418de84edecdaa7bbbeb34c2b85c2e25bb870
ordersam <46249762+ordersam@users.noreply.github.com>
2020-12-09 11:04:16 +0800

    畫圖

---
+++ 99.畫圖/C_Sharp畫空心三角形.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+
+            int layer = 10;
+            for (int i = 0; i < (layer-1); i++)
+                str += " ";
+            str += "*\n";
+
+            for (int i = 0; i < (layer-1); i++)
+            {
+                for (int j = 0; j < (layer-1 - i - 1); j++)
+                    str += " ";
+                str += "*";
+                for (int j = 0; j < i; j++)
+                    str += " ";
+                str += "*\n";
+            }
+
+            for (int i = 0; i < (layer - 2); i++)
+            {
+                for (int j = 0; j < (i + 1); j++)
+                    str += " ";
+                str += "*";
+                for (int j = 0; j < ((layer - 2) - i - 1); j++)
+                    str += " ";
+                str += "*\n";
+            }
+
+            for (int i = 0; i < (layer - 1); i++)
+                str += " ";
+            str += "*";
+
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
